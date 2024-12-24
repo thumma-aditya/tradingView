3f785137aedb5b1952eaa3eac035d06fde10128f
prime-rowdy <aditya.careermail@gmail.com>
2024-12-24 13:50:17 -0600

    Update trading.ts

--- src/trading.ts
+++ src/trading.ts
@@ -2,7 +2,7 @@ import { chromium } from 'playwright';
 import * as fs from 'fs';
 
 async function run() {
-  const browser = await chromium.launch({ headless: false });
+  const browser = await chromium.launch({ headless: true });
   const context = await browser.newContext();
   const page = await context.newPage();
 
